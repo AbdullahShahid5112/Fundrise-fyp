9317dbbead4b2bbc5286f59a98490031dccbc695
AbdullahShahid5112 <abdullahshahid5112@gmail.com>
2025-06-24 12:40:16 +0500

    fix: Correct import path for Industries component in LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import HeroSection from '../components/LandingPage/HeroSection'
 import PitchCards from '../components/LandingPage/PitchCards'
-import Industries from '../components/LandingPage/industries'
+import Industries from '../components/LandingPage/Industries'
 import CallToAction from '../components/LandingPage/CallToAction'
-import InvestorsCard from '../components/Investors/InvestorsCard'
+// import InvestorsCard from '../components/Investors/InvestorsCard'
 import Investor1 from '../components/Investors/Investor1'
 const LandingPage = () => {
   return (
